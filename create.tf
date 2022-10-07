dbc1ae79bbd0152c57ae8f945d9af7ae4f750f07
Paulo Vitor <paulovitor8@gmail.com>
2022-10-06 23:10:18 -0300

    Eliminando nat_gatway

--- create.tf
+++ create.tf
@@ -21,7 +21,7 @@ module "vpc" {
   azs                = var.vpc_azs
   private_subnets    = var.vpc_private_subnets
   public_subnets     = var.vpc_public_subnets
-  enable_nat_gateway = var.vpc_enable_nat_gateway
+  # enable_nat_gateway = var.vpc_enable_nat_gateway
   tags               = var.vpc_tags
 }
 
